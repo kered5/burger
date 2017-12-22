c5861a5ba165ef94e6bb3ddf8d837d238c5a5af9
kered5 <kered5@icloud.com>
2017-12-22 13:49:30 -0600

    Adding remote DB connection

--- config/connection.js
+++ config/connection.js
@@ -1,16 +1,24 @@
 // Database Connections
-//const Promises = require('bluebird');
+
 const MySQL = require('mysql');
-
-//Promises.promisifyAll(require("mysql/lib/Connection").prototype);
-//Promises.promisifyAll(require("mysql/lib/Pool").prototype);
-
+if(process.env.JAWSDB_URL){
+	burgers= MySQL.createConnection(process.env.JAWSDB_URL);
+}
+else{
 var burgers = MySQL.createConnection({
 	host: "localhost",
 	user: "root",
 	password: "root",
 	database: "burgers_db"
 });
+};
+
+// var burgers = MySQL.createConnection({
+// 	host: "localhost",
+// 	user: "root",
+// 	password: "root",
+// 	database: "burgers_db"
+// });
 
 var allConnections = {
 	'burgers_db' : burgers
